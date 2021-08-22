a9e6643a612b0c361ed70f6358c8bb5760c57c4b
Marry <2112791909@qq.com>
2021-08-22 10:26:02 +0800

    动态服务器

---
+++ test.js
@@ -0,0 +1,12 @@
+const fs=require('fs')
+//读数据库
+const usersString=fs.readFileSync('./db/users.json').toString()
+const usersArray=JSON.parse(usersString)
+console.log(usersString)
+console.log( typeof usersArray)
+
+//写数据库
+const user3={id:3,name:'tom',password:'yyy'}
+usersArray.push(user3)
+const string =JSON.stringify(usersArray)
+fs.writeFileSync('./db/users.json',string)
